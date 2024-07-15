a804c8e03e66ddeae153b98f9876ddfe9224d044
Thanh Pham <jonathanphamw22@gmail.com>
2024-07-14 22:02:44 -0700

    fixed background again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,12 @@ import { url } from "inspector";
 
 export default  async function Home() {
   const session = await auth();
-//   return (
-//     <div className="h-screen bg-cover bg-center" style={{backgroundImage: "url('/background.webp')"}}>
-//       <div className=" bg-center" style={{backgroundImage: "url('/vietkieu.webp')"}}></div>
-      
-//     </div>
-//   )
-// }
 return (
   <div className="relative h-screen bg-cover bg-center" style={{ backgroundImage: "url('/background.webp')" }}>
       
-      <div className="absolute inset-0 flex justify-center items-center bg-cover bg-center" style={{ width: '700px', height: '700px', backgroundImage: "url('/vietkieu.webp')" }}></div>
+      <div className="absolute inset-0 flex justify-center items-center">
+      <div className="bg-cover bg-center" style={{ width: '700px', height: '700px', backgroundImage: "url('/vietkieu.webp')" }}></div>
+      </div>
     
   </div>
 );
