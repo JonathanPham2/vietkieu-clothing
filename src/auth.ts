1d7b335f54c8d601be2780a81d3a5af25f079643
Thanh Pham <jonathanphamw22@gmail.com>
2024-08-04 01:43:11 -0700

    test 3

--- src/auth.ts
+++ src/auth.ts
@@ -7,7 +7,7 @@ const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 
 if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
-    throw new Error("Missing google oauth credentials")
+    throw new Error("Missing google oauth credentias")
 }
 
 export const {handlers: {GET, POST}, auth, signOut, signIn} = NextAuth({
